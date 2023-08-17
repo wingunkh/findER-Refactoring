85b416f6f2b8f44cf6cb8344fd1c616667504c04
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-08-17 11:44:26 +0900

    feat: BaseEntity 생성
    클래스들 간에 공통된 생성일 및 수정일 정보를 관리하는 클래스

--- src/main/java/com/finder/domain/User.java
+++ src/main/java/com/finder/domain/User.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "users")
-public class User {
+public class User extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
