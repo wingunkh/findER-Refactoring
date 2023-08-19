fb2fbb706b4bf1c9cc41259a6c9b42a478e32f6d
wingunkh <99gusrms@naver.com>
2023-08-19 15:54:55 +0900

    feat : Questionnaire 관련 클래스 추가

---
+++ src/main/java/com/finder/repository/QuestionnaireRepository.java
@@ -0,0 +1,7 @@
+package com.finder.repository;
+
+import com.finder.domain.Questionnaire;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionnaireRepository extends JpaRepository<Questionnaire, Long> {
+}
