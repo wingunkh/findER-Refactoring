9256fa5577e6bdf9f7630ba352dd943d02d665f1
wingunkh <99gusrms@naver.com>
2023-08-19 21:01:20 +0900

    feat : 문진표 작성 기능 구현

--- src/main/java/com/finder/controller/QuestionnaireController.java
+++ src/main/java/com/finder/controller/QuestionnaireController.java
@@ -1,6 +1,11 @@
 package com.finder.controller;
 
+import com.finder.dto.QuestionnaireDto;
+import com.finder.service.QuestionnaireService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/questionnaire")
 public class QuestionnaireController {
+    private final QuestionnaireService questionnaireService;
+
+    @PostMapping("/{id}")
+    public ResponseEntity writeQuestionnaire(@RequestBody QuestionnaireDto questionnaireDto) {
+        return ResponseEntity.ok(questionnaireService.writeQuestionnaire(questionnaireDto));
+    }
 }
