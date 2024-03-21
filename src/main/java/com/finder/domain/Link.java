090d620a0194cddfe46d627613d6731dc3b2e2db
wingunkh <99gusrms@naver.com>
2024-03-21 15:15:22 +0900

    comment: 주석 추가

--- src/main/java/com/finder/domain/Link.java
+++ src/main/java/com/finder/domain/Link.java
@@ -13,11 +13,11 @@ public class Link {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "LINK_SEQUENCE_GENERATOR")
     @SequenceGenerator(name = "LINK_SEQUENCE_GENERATOR", sequenceName = "LINK_SQ", initialValue = 1, allocationSize = 1)
-    private Long id;
+    private Long id; // 기본키
 
     @ManyToOne
-    private Account account1;
+    private Account account1; // 연동 계정1
 
     @ManyToOne
-    private Account account2;
+    private Account account2; // 연동 계정2
 }
