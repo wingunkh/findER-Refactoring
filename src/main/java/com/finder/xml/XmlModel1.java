23f24a4770f90c70d269bc71f4b191a15f8e80a8
wingunkh <99gusrms@naver.com>
2024-04-09 15:39:02 +0900

    fix: XmlModel 수정

---
+++ src/main/java/com/finder/xml/XmlModel1.java
@@ -0,0 +1,62 @@
+package com.finder.xml;
+
+import lombok.Data;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@Data
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "response")
+public class XmlModel1 {
+    @XmlElement(name = "body")
+    private Body body;
+
+    @Data
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Body {
+        @XmlElement(name = "items")
+        private Items items;
+    }
+
+    @Data
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Items {
+        @XmlElement(name = "item")
+        private List<Item> item;
+    }
+
+    @Data
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Item {
+        // 기관 코드
+        @XmlElement(name = "hpid")
+        private String hpid;
+
+        // 병상 수
+        @XmlElement(name = "hvec")
+        private Integer hvec;
+
+        // 응급실 당직의 직통 연락처
+        @XmlElement(name = "hv1")
+        private String hv1;
+
+        // 소아 당직의 직통 연락처
+        @XmlElement(name = "hv12")
+        private String hv12;
+
+        // 구급차 가용 여부
+        @XmlElement(name = "hvamyn")
+        private String hvamyn;
+
+        // CT 가용 여부
+        @XmlElement(name = "hvctayn")
+        private String hvctayn;
+
+        // MRI 가용 여부
+        @XmlElement(name = "hvmriayn")
+        private String hvmriayn;
+    }
+}
---
+++ src/main/java/com/finder/xml/XmlModel2.java
@@ -0,0 +1,62 @@
+package com.finder.xml;
+
+import lombok.Data;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@Data
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "response")
+public class XmlModel2 {
+    @XmlElement(name = "body")
+    private Body body;
+
+    @Data
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Body {
+        @XmlElement(name = "items")
+        private Items items;
+    }
+
+    @Data
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Items {
+        @XmlElement(name = "item")
+        private List<Item> item;
+    }
+
+    @Data
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Item {
+        // 기관명
+        @XmlElement(name = "dutyName")
+        private String dutyName;
+
+        // 주소
+        @XmlElement(name = "dutyAddr")
+        private String dutyAddr;
+
+        // 대표 전화
+        @XmlElement(name = "dutyTel1")
+        private String dutyTel1;
+
+        // 응급실 전화
+        @XmlElement(name = "dutyTel3")
+        private String dutyTel3;
+
+        // 간이 약도
+        @XmlElement(name = "dutyMapimg")
+        private String dutyMapimg;
+
+        // 병원 경도
+        @XmlElement(name = "wgs84Lon")
+        private String wgs84Lon;
+
+        // 병원 위도
+        @XmlElement(name = "wgs84Lat")
+        private String wgs84Lat;
+    }
+}
