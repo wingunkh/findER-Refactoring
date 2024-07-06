233b8210f0d229e7f56d140fdefbcd731f0b9322
wingunkh <99gusrms@naver.com>
2024-07-06 21:46:27 +0900

    comment: 주석 추가

--- src/main/java/com/finder/FinderApplication.java
+++ src/main/java/com/finder/FinderApplication.java
@@ -15,6 +15,10 @@ public class FinderApplication {
 		SpringApplication.run(FinderApplication.class, args);
 	}
 
+	// 스프링 빈의 이벤트 라이프사이클
+	// 스프링 컨테이너 생성 → 스프링 빈 생성 → 의존관계 주입 → 초기화 콜백 → 사용 → 소멸 전 콜백 → 스프링 종료
+	// @PostConstruct : 초기화 콜백 (빈이 생성되고, 빈의 의존관계 주입이 완료된 후 호출)
+	// @PreDestroy : 소멸 전 콜백 (빈이 소멸되기 직전에 호출)
 	@PostConstruct
 	public void init() {
 		publicDataAPIService.updateEmergencyRoomInfo();
