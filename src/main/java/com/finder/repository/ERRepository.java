58dba44e7325fc98727795b3889f397d53095b15
wingunkh <99gusrms@naver.com>
2024-07-10 22:14:36 +0900

    style: 메서드 없는 클래스 코드 포맷 변경

--- src/main/java/com/finder/repository/ERRepository.java
+++ src/main/java/com/finder/repository/ERRepository.java
@@ -3,9 +3,6 @@ package com.finder.repository;
 import com.finder.domain.ER;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
-public interface ERRepository extends JpaRepository<ER, String> {
-    List<ER> findByLatitudeBetweenAndLongitudeBetween(Double swLat, Double neLat, Double swLon, Double neLon);
-}
+public interface ERRepository extends JpaRepository<ER, String> {}
